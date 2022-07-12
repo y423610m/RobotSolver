9dd933425ef1fb15ffca3575973644b1fe579193
y423610m <y423610m@gmail.com>
2022-07-13 04:34:48 +0900

    enabled ros commands

--- robot_solver/src/main.cpp
+++ robot_solver/src/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 using namespace std;
 
-#include <ros/ros.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "solver");
     ros::NodeHandle nh;
 
-    
+    ros::Rate loop_rate(10);
+
+    ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+    std_msgs::String msg;
+    msg.data = "nenecchi";
 
     cout<<"hello world"<<endl;
     while(ros::ok()){
 
+        chatter_pub.publish(msg);
+
+        ros::spinOnce();
+        loop_rate.sleep();
     }
     return 0;
 }
+
