fb697cb2c7a864dd460cd7a16640010b5fe56fb7
y423610m <y423610m@gmail.com>
2022-07-14 19:04:28 +0900

    enabled to get actual robot joint angles

--- build/robot_solver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Desktop/robot_solver_ws/src/robot_solver
-# Build directory: /home/ubuntu/Desktop/robot_solver_ws/src/build/robot_solver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
